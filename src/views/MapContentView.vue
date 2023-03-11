804ad284b6e54e7d81fb8c0ce385ac3fd981fb2f
Pasha-programmer <pavelmikryukov2001@gmail.com>
2023-03-11 18:57:33 +0400

    Clear code and small optimizated

--- src/views/MapContentView.vue
+++ src/views/MapContentView.vue
@@ -1,104 +1,101 @@
-npm<script setup lang="ts">
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import colorIzhToEnd from '../colorIzhevskToEnd.json'
-import colorMscToIzh from '../colorMoscowToIzhevsk.json'
+npm
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import { RouterLink } from 'vue-router';
+    import colorIzhToEnd from '../colorIzhevskToEnd.json'
+    import colorMscToIzh from '../colorMoscowToIzhevsk.json'
 
-//state
-const colorsLevelMsc = ref(colorMscToIzh) // array all color
-const colorsLevelIzh = ref(colorIzhToEnd)
-const radiusIzh = 20;
-const currDistanse = ref(0)
-const distanseIzhToMsc = 969; // maybe other value
-const latitudEnd = 56.8498;
-const longitudeEnd = 53.2045;
-const color = ref('')
+    //state
+    const colorsLevelMsc = ref(colorMscToIzh) // array all color
+    const colorsLevelIzh = ref(colorIzhToEnd)
+    const radiusIzh = 20;
+    const currDistanse = ref(0)
+    const distanseIzhToMsc = 969; // maybe other value
+    const latitudEnd = 56.8498;
+    const longitudeEnd = 53.2045;
+    const color = ref('')
 
-//methods
-// Get position of the device, that using your site
-const getCurrentPosition = () => {
+    //methods
+    // Get position of the device, that using your site
+    const getCurrentPosition = () => {
 
-    const success = (position) => {
-        if(currDistanse.value == distance(position.coords.latitude, position.coords.longitude, latitudEnd,longitudeEnd)){
-            return;
+        const success = (position) => {
+            if(currDistanse.value == distance(position.coords.latitude, position.coords.longitude, latitudEnd,longitudeEnd)){
+                console.log('watchPosition.return')
+                return;
+            }
+
+            currDistanse.value = distance(position.coords.latitude, position.coords.longitude, latitudEnd,longitudeEnd);
+            if (currDistanse.value > radiusIzh){
+                const indexColor = Math.round(currDistanse.value / (distanseIzhToMsc / colorsLevelMsc.value.length))
+                color.value = colorsLevelMsc.value[indexColor].color; 
+                console.log(indexColor)
+            } 
+            else {
+                console.log(currDistanse.value)
+                const indexColor = Math.round(currDistanse.value / (radiusIzh / colorsLevelIzh.value.length))
+                color.value = colorsLevelIzh.value[indexColor].color; 
+                console.log(indexColor)
+            }
+            console.log('watchPosition.suc')
         }
-            
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
 
-        currDistanse.value = distance(latitude, longitude, latitudEnd,longitudeEnd);
-        if (currDistanse.value > radiusIzh){
-            const indexColor = Math.round(currDistanse.value / (distanseIzhToMsc / colorsLevelMsc.value.length))
-            color.value = colorsLevelMsc.value[indexColor]['color'] as string; 
-        } 
-        else {
-            console.log(currDistanse.value)
-            const indexColor = Math.round(currDistanse.value / (radiusIzh / colorsLevelIzh.value.length))
-            color.value = colorsLevelIzh.value[indexColor].color as string; 
-            console.log(indexColor)
+        const error = () => {
+            //TODO: if don't get position
+            console.log('watchPosition.error') 
         }
-        console.log('getCurrentPosition.suc')
+
+        navigator.geolocation.watchPosition(success, error, {maximumAge: 0, timeout: 300, enableHighAccuracy: true})
+        console.log('watchPosition.call')
     }
 
-    const error = () => {
-        //TODO: if don't get position
-        console.log('Не могу найти тебя') 
+    // Get from degrees to radians
+    const convertDeg2rad = (num: number) => {
+        return num * Math.PI / 180;
     }
-    navigator.geolocation.watchPosition(success, error, {maximumAge: 0, enableHighAccuracy: true})
-    console.log('getCurrentPosition')
-}
 
-// Get from degrees to radians
-const convertDeg2rad = (num: number) => {
-    return num * Math.PI / 180;
-}
+    // Get distanse between two coordinates to kilometers.
+    const distance = (lat_2:number, lon_2:number, lat_1: number, lon_1:number) => {
 
-// Get distanse between two coordinates to kilometers.
-const distance = (lat_2:number, lon_2:number, lat_1: number, lon_1:number) => {
+        const radius_earth = 6371.1; // Радиус Земли
 
-    const radius_earth = 6371.1; // Радиус Земли
+        const lat_1R = convertDeg2rad(lat_1),
+            lon_1R = convertDeg2rad(lon_1),
+            lat_2R = convertDeg2rad(lat_2),
+            lon_2R = convertDeg2rad(lon_2);
 
-    const lat_1R = convertDeg2rad(lat_1),
-        lon_1R = convertDeg2rad(lon_1),
-        lat_2R = convertDeg2rad(lat_2),
-        lon_2R = convertDeg2rad(lon_2);
+        const d = 2 * radius_earth * Math.asin(Math.sqrt(Math.sin((lat_2R - lat_1R) / 2) ** 2 + Math.cos(lat_1R) * Math.cos(lat_2R) * Math.sin((lon_2R - lon_1R) / 2) ** 2));
 
-    const d = 2 * radius_earth * Math.asin(Math.sqrt(Math.sin((lat_2R - lat_1R) / 2) ** 2 + Math.cos(lat_1R) * Math.cos(lat_2R) * Math.sin((lon_2R - lon_1R) / 2) ** 2));
+        return Math.round(d*1000)/1000; // round to 3 number after comma
+    }
 
-    return Math.round(d*1000)/1000; // round to 3 number after comma
-}
+    const getColor = () => {
+        getCurrentPosition();
+        return color.value
+    }
 
-const getColorByPosition = () => {
-    getCurrentPosition()  
-}
+    const getColorBar =(coef: number) => {
+        let rgb = hexToRgb(color.value);
+        rgb = {r: Number(rgb?.r) -coef, g: Number(rgb?.g)-coef, b: Number(rgb?.b)-coef,}
+        return rgbToHex(rgb.r,rgb.g,rgb.b);
+    }
 
-const getColor = () => {
-    getColorByPosition();
-    return color.value
-}
+    function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+    function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+    }
 
-const getColorBar =(coef: number) => {
-    let rgb = hexToRgb(getColor());
-    rgb = {r: Number(rgb?.r) -coef, g: Number(rgb?.g)-coef, b: Number(rgb?.b)-coef,}
-    return rgbToHex(rgb.r,rgb.g,rgb.b);
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
-}
+    function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+    } : null;
+    }
 </script>
 
 <template>
@@ -108,7 +105,8 @@ function hexToRgb(hex) {
         <RouterLink to="/" class="rl"><h2 class="link-title" :style="{background: getColorBar(10)}">Shmavgeniy</h2></RouterLink>
     </div>
 </nav>
-        <p class="distance__map-block" :style="{background: getColor()}">{{ currDistanse }} km</p>
+        <!-- <p class="distance__map-block" :style="{background: getColor()}">{{ currDistanse }} km</p> -->
+        <p class="distance__map-block">{{ currDistanse }} km</p>
         <!--<p v-for="colors in colorsLevel"> {{ colors }}</p>-->
     </main>
 </template> 
